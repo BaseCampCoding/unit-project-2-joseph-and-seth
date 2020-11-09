b7d015dabde70bf7829f496fa928d88fbaf05775
Seth Hogue <shogue21@basecampcodingacademy.org>
2020-11-09 14:20:40 -0600

    Added support for random date generation

--- customer.py
+++ customer.py
@@ -1,3 +1,18 @@
+from datetime import date
+import random
+
+time_choices = ["4:00", "4:30", "5:00", "5:30", "6:00", "6:30", "7:00", "7:30", "8:00"]
+
+def random_date():
+    start_dt = date.today().toordinal()
+    end_dt = date(2020, 12, 31).toordinal()
+    random_day = date.fromordinal(random.randint(start_dt, end_dt))
+    return random_day
+
+def choose_times():
+    random_times = random.choices(time_choices)
+    return random_times
+
 print("Welcome to Food Line! Home to all your restaraunt needs!")
 choice = input('''Would you like to:
 - Make a [reservation]
@@ -7,5 +22,15 @@ choice = input('''Would you like to:
 if choice == "reservation":
     name = input("What is the name for this order?\n")
     count = input("What is the size of your party?\n")
-    print('''Here are our available dates and times.
-    ''')
+    print('Here are our available dates and times.')
+    dates = {}
+    for i in range(10):
+        dates["Date %s" %i] = random_date()
+    for key in dates:
+        print(dates[key])
+    chosen_date = input("What date would you like to schedule for?\n")
+    split_dates = chosen_date.split("-")
+    chosen_date = date(int(split_dates[0]), int(split_dates[1]), int(split_dates[2]))
+    if chosen_date in dates.values():
+        for time in range(len(choose_times())):
+            print(time)
