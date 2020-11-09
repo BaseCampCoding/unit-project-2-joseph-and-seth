3283c48d1412297982049ac4cd6a9d0ba895e7c5
joseph dandan <jdandan21@basecampcodingacademy.org>
2020-11-09 14:20:49 -0600

    Created more tables for the program.

--- db_setup.py
+++ db_setup.py
@@ -1,9 +1,17 @@
 import sqlite3
 import datetime
 
-con = sqlite3.connect('resteraunt_1.db')
+con = sqlite3.connect('restaraunt_1.db')
 cur = con.cursor()
 
-cur.execute('CREATE TABLE IF NOT EXISTS Reservations(Name TEXT, Party-Count INTEGER, Date timestamp, Order REAL)')
+cur.execute('CREATE TABLE IF NOT EXISTS Reservations(Name TEXT, Party-Count INTEGER, Date timestamp, Time TIME, Total REAL)')
 
-cur.execute('CREATE TABLE IF NOT EXISTS Menu(Name TEXT, Item REAL')
+cur.execute('CREATE TABLE IF NOT EXISTS Menu(Name TEXT, Price REAL)')
+
+cur.execute('CREATE TABLE IF NOT EXISTS Carry-out(Name TEXT, Total REAL)')
+
+cur.execute('CREATE TABLE IF NOT EXISTS Delivery(Name TEXT, Destination TEXT, Total REAL)')
+
+cur.execute('CREATE TABLE IF NOT EXISTS Availability(Available INTEGER)')
+
+con.commit()
