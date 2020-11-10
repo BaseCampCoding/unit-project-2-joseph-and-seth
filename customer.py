c15d35b2d8fe054846bf54d7a762aebfc0b3561d
Seth Hogue <shogue21@basecampcodingacademy.org>
2020-11-10 09:43:41 -0600

    Almost finished with reservation option, need improvements

--- customer.py
+++ customer.py
@@ -3,49 +3,79 @@ import random
 import sqlite3
 
 time_choices = ["4:00", "4:30", "5:00", "5:30", "6:00", "6:30", "7:00", "7:30", "8:00"]
+dates = {}
 
-con = sqlite3.connect('restaraunt_1.db')
+con = sqlite3.connect("restaraunt_1.db")
 cur = con.cursor()
 
-def random_date():
+
+def random_dates():
+    global dates
     start_dt = date.today().toordinal()
     end_dt = date(2020, 12, 31).toordinal()
     random_day = date.fromordinal(random.randint(start_dt, end_dt))
-    return random_day
+    for i in range(10):
+        dates["Date %s" % i] = random_day
+    for key in dates:
+        print(f"{dates[key]}")
+
 
 def choose_times():
     random_times = random.choices(time_choices)
     return random_times
 
+
+def menu():
+    cur.execute("SELECT * FROM Menu")
+    for row in cur.fetchall():
+        print(f"Item: {row[0]}, Price: {row[1]}")
+
+
 print("Welcome to Food Line! Home to all your restaraunt needs!")
-choice = input('''Would you like to:
+choice = input(
+    """Would you like to:
 - Make a [reservation]
 - Order [carry-out]
 - Schedule a [delivery]
-''')
+"""
+)
 if choice == "reservation":
-    name = input("What is the name for this order?\n")
-    count = input("What is the size of your party?\n")
-    print('Here are our available dates and times.')
+    name = input("\nWhat is the name for this order?\n")
+    count = input("\nWhat is the size of your party?\n")
+    print("\nHere are our available dates.\n")
 
-    dates = {}
-    for i in range(10):
-        dates["Date %s" %i] = random_date()
-    for key in dates:
-        print(dates[key])
+    random_dates()
 
-    chosen_date = input("What date would you like to schedule for?\n")
+    chosen_date = input("\nWhat date would you like to schedule for?\n")
     split_dates = chosen_date.split("-")
     chosen_date = date(int(split_dates[0]), int(split_dates[1]), int(split_dates[2]))
 
-    print("Here are available times.")
+    print("\nHere are available times.\n")
     if chosen_date in dates.values():
         times = {}
         for i in range(6):
-            times["Time %s" %i] = choose_times()
+            times["Time %s" % i] = choose_times()
         for key in times:
-            print(times[key])
+            print(f"{times[key]}")
 
-    chosen_time = input("What time would you like to schedule for?\n")
-    cur.execute('INSERT INTO Reservations VALUES (?, ?, ?, ?, ?)', (name, count, chosen_date, chosen_time, None))
+    chosen_time = input("\nWhat time would you like to schedule for?\n")
+    cur.execute(
+        "INSERT INTO Reservations VALUES (?, ?, ?, ?, ?)",
+        (name, count, chosen_date, chosen_time, None),
+    )
+    menu()
+    total = 0
+    while True:
+        food_choice = input("What would you like to order?\n")
+        sqlStatement = f"SELECT Price FROM Menu WHERE Item = {food_choice}"
+        cur.execute(sqlStatement)
+        total += cur.fetchone()
+        choose_more = input("Would you like to add any more items?\n")
+        if choose_more.lower() == "y":
+            menu()
+        elif choose_more.lower() == "n":
+            break
+        else:
+            print("Please choose a valid option")
+
     con.commit()
