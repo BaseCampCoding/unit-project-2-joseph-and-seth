917d3ba86d1ea4a139f0c3ed7e39214b8ab399c1
joseph dandan <jdandan21@basecampcodingacademy.org>
2020-11-10 14:13:16 -0600

    Changed qol added comments

--- restaraunt.py
+++ restaraunt.py
@@ -6,56 +6,75 @@ con = sqlite3.connect('restaraunt_1.db')
 cur = con.cursor()
 
 
-
+# Presents user with menu options of what they can do.
 print("Food Line Staff Menu")
 action = input('''Would you like to:
 - View queued reservations select [View]
 - Change menu select [Menu]
 - Check availability for a guest/guests on the waiting list select [Availability]
-- Quit the menu select [Quit]
+- Shutdowns the device [Shutdown]
 ''')
 
+
+# Views the reservations in the queue.
 if action.lower() == "view":
     cur.execute('SELECT * FROM Reservations')
     for row in cur.fetchall():
         print(f"Name: {row[0]}, Party Count: {row[1]}, Date: {row[2]}, Time: {row[3]}, Total: {row[4]}\n")
+# If the user chooses menu they should have options of what they might want to do the the menu.
 elif action.lower() == "menu":
     choice = input("""What option would you like to select?:
 - View the current menu [View]
 - Change the current menu [Change]
 - Add to the current menu [Add]
 - Delete from the current menu [Delete]
+- Quit the menu select [Quit]
 """)
+# Should allow the user be able to see the current menu or changes to the menu.
     if choice.lower() == "view":
         cur.execute('SELECT * FROM Menu')
         for row in cur.fetchall():
             print(f"ID: {row[0]}, Item: {row[1]}, Price: {row[2]}")
+# Should allow the user to change the menu.
     elif choice.lower() == "change":
         id_number = input("What is the ID of the item you are changing? ")
         change_option = input("What value would you like to change? ")
+# The user can change the item
         if change_option.lower() == "item":
             new_name = input("What would you like to change this item's name to? ")
             cur.execute('UPDATE Menu SET Item = ? WHERE ID = ?', [new_name, id_number])
             con.commit()
+# The user can change the price
         elif change_option.lower() == "price":
             new_price = input("What would you like to change this item's price to? ")
             cur.execute('UPDATE Menu SET Price = ? WHERE ID = ?', [new_price, id_number])
             con.commit()
+# The user can add a new item, name of the item, and price of the item.
     elif choice.lower() == "add":
         item_id = input('What will be the ID of the new item? ')
         item_name = input('What will be the name of the new item? ')
         item_price = input('What will be the new price of the item? ')
         cur.execute('INSERT INTO Menu VALUES (?, ?, ?)', (item_id, item_name, item_price))
         con.commit()
+# The user can delete an item.
     elif choice.lower() == "delete":
         del_item_id = input('What is the ID you want to delete? ')
         cur.execute('DELETE FROM Menu WHERE ID = ?', [del_item_id])
         con.commit()
+# The user can quit to the main menu.
+    elif choice.lower() == "quit":
+        print("Heading back to the home menu")
 
+
+# The user can see the times available for a reservation.
 elif action.lower() == "availability":
+    cur.execute('SELECT * FROM ReservationTimes')
+    for row in cur.fetchall():
+        print(f"Date: {row[0]}, Time: {row[1]}")
+# The user can change what times and dates are available and are removed from the list or added back.
     availability_choice = input("""What would you like to change about availability?
-    [1] Set Availability
-    [2] Provide new available times.
+[1] Set Availability
+[2] Provide new available times.
     """)
     if availability_choice == 1:
         cur.execute('SELECT Available FROM Availability')
@@ -76,8 +95,9 @@ elif action.lower() == "availability":
         else:
             print("Please choose a valid option.")
     elif availability_choice == 2:     
-        print("") 
-elif action.lower() == "quit":
+        print("")
+# Turns off the device. 
+elif action.lower() == "Shutdown":
     print("Shutting down.")       
     
 
