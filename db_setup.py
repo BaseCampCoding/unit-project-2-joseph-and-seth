5b2ddaa7f8355088c77ab4138c033d1cd8165c33
joseph dandan <jdandan21@basecampcodingacademy.org>
2020-11-10 08:41:22 -0600

    Removed dashes to fix bug

--- db_setup.py
+++ db_setup.py
@@ -4,11 +4,11 @@ import datetime
 con = sqlite3.connect('restaraunt_1.db')
 cur = con.cursor()
 
-cur.execute('CREATE TABLE IF NOT EXISTS Reservations(Name TEXT, Party-Count INTEGER, Date timestamp, Time TIME, Total REAL)')
+cur.execute('CREATE TABLE IF NOT EXISTS Reservations(Name TEXT, PartyCount INTEGER, Date timestamp, Time TIME, Total REAL)')
 
 cur.execute('CREATE TABLE IF NOT EXISTS Menu(Name TEXT, Price REAL)')
 
-cur.execute('CREATE TABLE IF NOT EXISTS Carry-out(Name TEXT, Total REAL)')
+cur.execute('CREATE TABLE IF NOT EXISTS Carryout(Name TEXT, Total REAL)')
 
 cur.execute('CREATE TABLE IF NOT EXISTS Delivery(Name TEXT, Destination TEXT, Total REAL)')
 
