223c073f03e303297855600ed8fc63eed2a18383
Seth Hogue <shogue21@basecampcodingacademy.org>
2020-11-11 09:41:12 -0600

    Added carry-out and delivery options

--- customer.py
+++ customer.py
@@ -1,4 +1,5 @@
 from datetime import date
+from datetime import datetime
 import random
 import sqlite3
 from colorama import Style
@@ -19,7 +20,43 @@ def choose_times():
 def menu():
     cur.execute("SELECT * FROM Menu")
     for row in cur.fetchall():
-        print(f"ID: {row[0]}, Item: {row[1]}, Price: {row[2]}")
+        print(
+            f"{Fore.BLUE}ID{Style.RESET_ALL}: {row[0]}, {Fore.YELLOW}Item{Style.RESET_ALL}: {row[1]}, Price: {row[2]}"
+        )
+
+
+def get_time():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M")
+    return current_time
+
+
+def delivery_time():
+    current_time = get_time()
+    time = random.randint(30, 120)
+    delivery_time = f"{(int(current_time[1]) + (time // 60))}:{(int(current_time[3]) + (time % 60))}"
+    print(f"Your estimated delivery time will be {delivery_time}")
+    return delivery_time
+
+
+def menu_options():
+    menu()
+    total = 0
+    while True:
+        id_choice = input("\nChoose the ID of the item you want to order.\n")
+        sqlStatement = "SELECT Price FROM Menu WHERE ID = ?"
+        cur.execute(sqlStatement, [id_choice])
+        for price in cur.fetchall():
+            total += price[0]
+        print(f"\nYour total is currently {total:.2f}")
+        choose_more = input("\nWould you like to add any more items?\n")
+        if choose_more.lower() == "y":
+            menu()
+        elif choose_more.lower() == "n":
+            break
+        else:
+            print("Please choose a valid option")
+    return total
 
 
 print(
@@ -28,17 +65,17 @@ print(
 cur.execute("SELECT Available FROM Availability")
 availability = cur.fetchone()
 if availability == (1,):
-    print("Reservations are currently available")
+    print(f"Reservations are currently {Fore.GREEN}available{Style.RESET_ALL}.")
 else:
-    print("Reservations are currently unavailable.")
+    print(f"Reservations are currently {Fore.RED}unavailable{Style.RESET_ALL}.")
 choice = input(
     f"""Would you like to:
-- Make a [{Fore.BLUE}reservation{Style.RESET_ALL}]
-- Order [{Fore.RED}carry-out{Style.RESET_ALL}]
-- Schedule a [{Fore.YELLOW}delivery{Style.RESET_ALL}]
+[1] Make a {Fore.BLUE}reservation{Style.RESET_ALL}
+[2] Order {Fore.RED}carry-out{Style.RESET_ALL}
+[3] Schedule a {Fore.YELLOW}delivery{Style.RESET_ALL}
 """
 )
-if choice == "reservation" and availability == (1,):
+if choice == "1" and availability == (1,):
     name = input("\nWhat is the name for this order?\n")
     count = input("\nWhat is the size of your party?\n")
     print("\nHere are our available dates.\n")
@@ -61,26 +98,30 @@ if choice == "reservation" and availability == (1,):
     cur.execute("DELETE FROM ReservationTimes WHERE Date = ?", [chosen_date])
 
     chosen_time = input("\nWhat time would you like to schedule for?\n")
-    menu()
-    total = 0
-    while True:
-        id_choice = input("\nChoose the ID of the item you want to order.\n")
-        sqlStatement = "SELECT Price FROM Menu WHERE ID = ?"
-        cur.execute(sqlStatement, [id_choice])
-        for price in cur.fetchall():
-            total += price[0]
-        print(f"\nYour total is currently {total:.2f}")
-        choose_more = input("\nWould you like to add any more items?\n")
-        if choose_more.lower() == "y":
-            menu()
-        elif choose_more.lower() == "n":
-            break
-        else:
-            print("Please choose a valid option")
+    total = menu_options()
     cur.execute(
         "INSERT INTO Reservations VALUES (?, ?, ?, ?, ?)",
         (name, count, chosen_date2, chosen_time, total),
     )
     con.commit()
-elif choice == "reservation" and availability != (1,):
+elif choice == "1" and availability != (1,):
     print("We're sorry, reservations are currently unavailable at this time.")
+elif choice == "2":
+    name = input("\nWhat is the name for this order?\n")
+    time = input("\nWhat time do you want to pick it up?\n")
+
+    total = menu_options()
+
+    cur.execute("INSERT INTO Carryout VALUES (?, ?, ?)", (name, time, total))
+    con.commit()
+elif choice == "3":
+    name = input("What is the name for this order?")
+    address = input("What is the address you want to deliver to?")
+    delivery_time = delivery_time()
+    total = menu_options()
+
+    cur.execute(
+        "INSERT INTO Delivery VALUES (?, ?, ?, ?)",
+        (name, address, delivery_time, total),
+    )
+    con.commit()
