4f8594c881b591a12e85126c4bcddb364aab0aab
Seth Hogue <shogue21@basecampcodingacademy.org>
2020-11-12 09:34:27 -0600

    Improved user UI and added more input validation

--- customer.py
+++ customer.py
@@ -1,11 +1,5 @@
 from functions import *
 
-Blue = Fore.BLUE
-Red = Fore.RED
-Yellow = Fore.YELLOW
-Green = Fore.GREEN
-Reset = Style.RESET_ALL
-
 con = sqlite3.connect("restaraunt_1.db")
 cur = con.cursor()
 
@@ -28,7 +22,7 @@ if choice == "1" and availability == (1,):
     name = input(f"\nWhat is the {Yellow}name{Reset} for this order?\n")
     count = input(f"\nWhat is the {Red}size{Reset} of your party?\n")
     print(f"\nHere are our {Green}available dates{Reset}.\n")
-    cur.execute("SELECT Date FROM ReservationTimes")
+    cur.execute("SELECT Date FROM ReservationTimes GROUP BY Date")
     for row in cur.fetchall():
         print(f"{Green}Date{Reset}: {row[0]}")
 
@@ -37,10 +31,10 @@ if choice == "1" and availability == (1,):
     chosen_date2 = date(int(split_dates[2]), int(split_dates[0]), int(split_dates[1]))
 
     print(f"\nHere are {Blue}available times{Reset}.\n")
-    cur.execute("SELECT Date FROM ReservationTimes")
+    cur.execute("SELECT DISTINCT Date FROM ReservationTimes")
     for row in cur.fetchall():
         if chosen_date in row:
-            cur.execute("SELECT Time FROM ReservationTimes")
+            cur.execute("SELECT DISTINCT Time FROM ReservationTimes GROUP BY Time")
             for row in cur.fetchall():
                 print(f"Time: {row[0]}")
 
@@ -49,6 +43,16 @@ if choice == "1" and availability == (1,):
     chosen_time = input("\nWhat time would you like to schedule for?\n")
     total = menu_options()
     order = json.dumps(items)
+    print(
+        f"\nThank you for using {Yellow}Food Line{Reset}! Here is your order summary!\n"
+    )
+    print(
+        f"""{Yellow}Name{Reset}: {name}, {Red}Party Count{Reset}: {count}
+{Green}Date{Reset}: {chosen_date2}, {Blue}Time{Reset}: {chosen_time}
+{Yellow}Order{Reset}: {order}
+{Green}Total{Reset}: ${total}
+    """
+    )
     cur.execute(
         "INSERT INTO Reservations VALUES (?, ?, ?, ?, ?, ?)",
         (name, count, chosen_date2, chosen_time, order, total),
@@ -62,6 +66,16 @@ elif choice == "2":
 
     total = menu_options()
     order = json.dumps(items)
+    print(
+        f"\nThank you for using {Yellow}Food Line{Reset}! Here is your order summary!\n"
+    )
+    print(
+        f"""{Yellow}Name{Reset}: {name}
+{Red}Time{Reset}: {time}
+{Blue}Order{Reset}: {order}
+{Green}Total{Reset}: ${total}
+    """
+    )
 
     cur.execute("INSERT INTO Carryout VALUES (?, ?, ?, ?)", (name, time, order, total))
     con.commit()
@@ -71,9 +85,21 @@ elif choice == "3":
     delivery_time = delivery_time()
     total = menu_options()
     order = json.dumps(items)
-
+    print(
+        f"\nThank you for using {Yellow}Food Line{Reset}! Here is your order summary!\n"
+    )
+    print(
+        f"""{Yellow}Name{Reset}: {name}
+{Red}Address{Reset}: {address}
+{Blue}Estimated Delivery Time{Reset}: {delivery_time}
+{Yellow}Order{Reset}: {order}
+{Green}Total{Reset}: ${total:.2f}
+    """
+    )
     cur.execute(
         "INSERT INTO Delivery VALUES (?, ?, ?, ?, ?)",
         (name, address, delivery_time, order, total),
     )
     con.commit()
+else:
+    print("Please choose a valid option!")
