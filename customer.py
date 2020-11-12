130a45c716ee15eaf95715364d6d585fbf1f1b0f
Seth Hogue <shogue21@basecampcodingacademy.org>
2020-11-12 10:28:02 -0600

    Added some support for selecting different restaurants

--- customer.py
+++ customer.py
@@ -1,24 +1,36 @@
 from functions import *
 
-con = sqlite3.connect("restaraunt_1.db")
-cur = con.cursor()
 
 print(f"Welcome to {Yellow}Food Line{Reset}! Home to all your restaraunt needs!")
-
+rest_choice = int(
+    input(
+        f"""Which restaurant would you like to place an order at?
+[1] {Red}Chilli's{Reset}
+[2] {Yellow}Outback Steakhouse{Reset}\n"""
+    )
+)
+if rest_choice == 1:
+    con = sqlite3.connect("Chillis.db")
+    cur = con.cursor()
+elif rest_choice == 2:
+    con = sqlite3.connect("Outback.db")
+    cur = con.cursor()
 cur.execute("SELECT Available FROM Availability")
 availability = cur.fetchone()
 if availability == (1,):
     print(f"Reservations are currently {Green}available{Reset}.")
 else:
     print(f"Reservations are currently {Red}unavailable{Reset}.")
-choice = input(
-    f"""Would you like to:
+choice = int(
+    input(
+        f"""Would you like to:
 [1] Make a {Blue}reservation{Reset}
 [2] Order {Red}carry-out{Reset}
 [3] Schedule a {Yellow}delivery{Reset}
 """
+    )
 )
-if choice == "1" and availability == (1,):
+if choice == 1 and availability == (1,):
     name = input(f"\nWhat is the {Yellow}name{Reset} for this order?\n")
     count = input(f"\nWhat is the {Red}size{Reset} of your party?\n")
     print(f"\nHere are our {Green}available dates{Reset}.\n")
@@ -41,7 +53,10 @@ if choice == "1" and availability == (1,):
     cur.execute("DELETE FROM ReservationTimes WHERE Date = ?", [chosen_date])
 
     chosen_time = input("\nWhat time would you like to schedule for?\n")
-    total = menu_options()
+    if rest_choice == 1:
+        total = menu_options1()
+    elif rest_choice == 2:
+        total = menu_options2()
     order = json.dumps(items)
     print(
         f"\nThank you for using {Yellow}Food Line{Reset}! Here is your order summary!\n"
@@ -58,13 +73,16 @@ if choice == "1" and availability == (1,):
         (name, count, chosen_date2, chosen_time, order, total),
     )
     con.commit()
-elif choice == "1" and availability != (1,):
+elif choice == 1 and availability != (1,):
     print("We're sorry, reservations are currently unavailable at this time.")
-elif choice == "2":
+elif choice == 2:
     name = input("\nWhat is the name for this order?\n")
     time = input("\nWhat time do you want to pick it up?\n")
 
-    total = menu_options()
+    if rest_choice == 1:
+        total = menu_options1()
+    elif rest_choice == 2:
+        total = menu_options2()
     order = json.dumps(items)
     print(
         f"\nThank you for using {Yellow}Food Line{Reset}! Here is your order summary!\n"
@@ -79,11 +97,14 @@ elif choice == "2":
 
     cur.execute("INSERT INTO Carryout VALUES (?, ?, ?, ?)", (name, time, order, total))
     con.commit()
-elif choice == "3":
+elif choice == 3:
     name = input("What is the name for this order?\n")
     address = input("What is the address you want to deliver to?\n")
     delivery_time = delivery_time()
-    total = menu_options()
+    if rest_choice == 1:
+        total = menu_options1()
+    elif rest_choice == 2:
+        total = menu_options2()
     order = json.dumps(items)
     print(
         f"\nThank you for using {Yellow}Food Line{Reset}! Here is your order summary!\n"
