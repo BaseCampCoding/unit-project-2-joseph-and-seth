958dbf74d3b04069e14ef369fc9689d1d56a3ae3
Seth Hogue <shogue21@basecampcodingacademy.org>
2020-11-13 09:57:30 -0600

    Visual Improvement

--- classes.py
+++ classes.py
@@ -1,6 +1,7 @@
 from variables import *
 
-class Reservation():
+
+class Reservation:
     def __init__(self, name, party_count, date, time, order, total):
         self.name = name
         self.party_count = party_count
@@ -8,43 +9,45 @@ class Reservation():
         self.time = time
         self.order = order
         self.total = total
-        
+
     def __str__(self):
         return f"""
 {Yellow}Name{Reset}: {self.name}, {Red}Party Count{Reset}: {self.party_count}
 {Green}Date{Reset}: {self.date}, {Blue}Time{Reset}: {self.time}
 {Yellow}Order{Reset}: {self.order}
-{Green}Total{Reset}: ${self.total}
+{Green}Total{Reset}: ${self.total:.2f}
         """
 
-class Carryout():
+
+class Carryout:
     def __init__(self, name, time, order, total):
         self.name = name
         self.time = time
         self.order = order
         self.total = total
-        
+
     def __str__(self):
         return f"""
 {Yellow}Name{Reset}: {self.name}
 {Blue}Time{Reset}: {self.time}
 {Yellow}Order{Reset}: {self.order}
-{Green}Total{Reset}: ${self.total}
+{Green}Total{Reset}: ${self.total:.2f}
 """
 
-class Delivery():
+
+class Delivery:
     def __init__(self, name, address, delivery_time, order, total):
         self.name = name
         self.address = address
         self.delivery_time = delivery_time
         self.order = order
         self.total = total
-        
+
     def __str__(self):
         return f"""
 {Yellow}Name{Reset}: {self.name}, 
 {Red}Address{Reset}: {self.address}
 {Blue}Estimated Delivery Time{Reset}: {self.delivery_time}
 {Yellow}Order{Reset}: {self.order}
-{Green}Total{Reset}: ${self.total}
+{Green}Total{Reset}: ${self.total:.2f}
 """
